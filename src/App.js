2a9955075bd2420bd40d96c0e200386b756a3f24
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-17 23:43:01 +0900

    クラスコンポーネント関数コンポーネントの演習

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './componets/Button/Button'
+
+
+function App () {
+  return (
+    <div>
+      <h1 className="title">
+        Hello, World
+      </h1>
+      <Button>ボタン１</Button>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/componets/Button/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.css';
+
+
+class Button extends React.Component {
+  render() {
+    return (
+      <span className="Button-container">
+        { this.props.children}
+      </span>
+
+    )
+  }
+}
+
+
+
+export default Button
