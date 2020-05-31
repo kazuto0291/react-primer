58313630dd29b59a0285d1116257cc774a5d64e9
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-01 01:37:44 +0900

    API経由のデータの取得前後の表示切り替え

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import Button from './componets/Button/Button';
-// import Timer from './componets/Timer/Timer';
-import Counter from './componets/Counter/Counter';
-import Loginout from './componets/Loginout/Loginout';
 
+const API_URL =' https://opentdb.com/api.php?amount=10';
 
-function App () {
-  return (
-    <div>
-      <h1 className="title">
-        Hello, World
-      </h1>
-      <Button>ボタン１</Button>
-      <h1>カウンター</h1>
-      <Counter />
-      <Loginout />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.requestData();
+  }
+
+  async requestData() {
+    this.setState({
+      loading: true
+    });
+
+    let firstQuizData;
+    try{
+      const response = await window.fetch(API_URL);
+      const data = await response.json();
+      firstQuizData = data.results[0];
+    } catch (error) {
+      firstQuizData = null;
+    }
+
+    this.setState({
+      loading: false,
+      data: firstQuizData
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.renderData()}
+        { this.renderRequestButton()}
+      </div>
+    )
+  }
+
+  renderData() {
+    if (this.state.loading) {
+      return <p>データ取得中...</p>;
+    }
+    if (!this.state.loading && !this.state.data) {
+      return <p>データなし</p>;
+    }
+    
+    return <p>{ JSON.stringify(this.state.data)} </p>
+  }
+
+  renderRequestButton() {
+    if (this.state.loading) {
+      return <button disabled>データ取得中....</button>;
+    }
+
+    return <button onClick={ () => {this.requestData()}}>データを取得する</button>
+  }
 }
 
 
